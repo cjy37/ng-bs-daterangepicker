402c9b300ab2f2e27c7c10074b4dc66b05789f1d
cjy37 <rocky.cn@foxmail.com>
2016-10-23 17:40:45 +0800

    update to utc datetime
    update to utc datetime

--- src/ng-bs-daterangepicker.js
+++ src/ng-bs-daterangepicker.js
@@ -1,5 +1,5 @@
 /**
- * @license ng-bs-daterangepicker v0.0.5
+ * @license ng-bs-daterangepicker v0.0.7
  * (c) 2013 Luis Farzati http://github.com/luisfarzati/ng-bs-daterangepicker
  * License: MIT
  */
